e656bdd30eac5944c6f269fb9714c3f75d2c7dbe
Fan Lin <FanL@sirius-io.com>
2025-03-22 09:00:38 +0000

    adding error cases

--- examples/devices/linemb/main.js
+++ examples/devices/linemb/main.js
@@ -2,12 +2,93 @@ import Timer from 'timer'
 
 trace('Hello, Linux Embedded!!!\n')
 
-Timer.set(id => trace('immediate\n'))
+// !!! Abort hook does NOT work !!!
+globalThis.abort = function (msg, exception) {
+	trace(`abort hook called with "${msg}"\n`);
+	trace(exception.stack, "\n");
 
-Timer.set(id => trace('oneshot\n'), 1000)
+	if (("unhandled exception" === msg) ||
+		("unhandled rejection" === msg))
+		return false;		// do not abort
+}
 
-let count = 0
-Timer.repeat(id => {
-	trace(`repeat ${count} \n`)
-	5 == ++count && Timer.clear(id)
-}, 1000)
+//
+// Error Handling Tests
+// !!! ONLY ENABLE ONE CASE AT A TIME !!!
+//
+
+// ***************************************************************************
+// Case 1: Uncaught exception from top level
+// ***************************************************************************
+// throw new Error('Uncaught exception from top level!')
+
+// =====OUTPUT======
+// Hello, Linux Embedded!!!
+// Error: unhandled exception
+// =================
+// (process exits)
+
+// ***************************************************************************
+// Case 2: Uncaught exception from setTimeout
+// ***************************************************************************
+// Timer.set(() => {
+// 	throw new Error('Uncaught exception from setTimeout!')
+// }, 1000)
+
+// =====OUTPUT======
+// Hello, Linux Embedded!!!
+// Error: unhandled exception
+// =================
+// Process exited with code
+
+
+// ***************************************************************************
+// Case 3: Promise rejection
+// ***************************************************************************
+// Promise.reject(new Error('Promise rejection!'))
+
+// =====OUTPUT======
+// Hello, Linux Embedded!!!
+// =================
+// Process keeps running
+
+// ***************************************************************************
+// Case 4: Uncaught exception from Promise
+// ***************************************************************************
+// new Promise(() => {
+// 	throw new Error('Uncaught exception from Promise!')
+// })
+
+// =====OUTPUT======
+// Hello, Linux Embedded!!!
+// =================
+// Process keeps running
+
+// ***************************************************************************
+// Case 5: Uncaught exception from setTimeout in Promise
+// ***************************************************************************
+// new Promise(() => {
+// 	Timer.set(() => {
+// 		throw new Error('Uncaught exception from setTimeout in Promise!')
+// 	}, 1000)
+// })
+
+// =====OUTPUT======
+// Hello, Linux Embedded!!!
+// Error: unhandled exception
+// =================
+// Process exited
+
+// ***************************************************************************
+// Case 6: Promise rejection in setTimeout
+// ***************************************************************************
+// new Promise((resolve, reject) => {
+// 	Timer.set(() => {
+// 		reject(new Error('Promise rejection in setTimeout!'))
+// 	}, 1000)
+// })
+
+// =====OUTPUT======
+// Hello, Linux Embedded!!!
+// =================
+// Process keeps running
